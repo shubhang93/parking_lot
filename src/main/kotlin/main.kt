dcf5e4fb89dfb0abb726e1f3a2e06461801087e1
shubhang <shubhang.sabbath@gmail.com>
2019-11-06 23:27:35 +0530

    feat: add test to park cars

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
-data class ParkingSpot(val position: Int, val car: Car)
+data class ParkingSpot(val position: Int)
 data class Car(val color: String, val licensePlate: String)
 
 enum class Command(val command: String) {
@@ -9,16 +9,31 @@ enum class Command(val command: String) {
 
 class ParkingLot(private val capacity: Int) {
     private val availableSpots = mutableListOf<ParkingSpot>()
+    private val occupiedSpots = mutableMapOf<Car, ParkingSpot>()
 
-    fun isParkingLotFull(): Boolean {
-        return availableSpots.size == capacity
+
+    init {
+        for (position in 0..capacity) {
+            availableSpots.add(ParkingSpot(position))
+        }
+        availableSpots.sortBy { it.position }
     }
+
+    private fun isParkingLotFull(): Boolean {
+        return availableSpots.size == 0
+    }
+
 
     fun parkCar(car: Car) {
-        val parkingSpot = ParkingSpot(1, car)
-        availableSpots.add(parkingSpot)
-        println("Parking Car with License Plate: ${car.licensePlate} and color: ${car.color} at position ${parkingSpot.position}")
+        if (!isParkingLotFull()) {
+            val parkingSpot = availableSpots.first()
+            availableSpots.removeAt(0)
+            occupiedSpots[car] = parkingSpot
+            println("Parking Car with License Plate: ${car.licensePlate} and color: ${car.color} at position ${parkingSpot.position}")
+        } else
+            println("Cannot Park call, all parking slots are full")
     }
+
 
 }
 
---
+++ src/test/kotlin/ParkingLotTest.kt
@@ -0,0 +1,42 @@
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+
+class ParkingLotTest {
+
+    private val parkingLot = ParkingLot(6);
+    private val outContent = ByteArrayOutputStream()
+    private val originalOut = System.out
+
+
+    @Before
+    fun setUpStreams() {
+        System.setOut(PrintStream(outContent))
+    }
+
+
+    fun restoreStreams() {
+        System.setOut(originalOut)
+    }
+
+    @Test
+    fun itShouldParkCars() {
+        val cars = listOf(
+            Car("White", "KA-02-EU8901"),
+            Car("Blue", "KA-01-AE9099"),
+            Car("Yellow", "KA-02-DV8900"),
+            Car("Orange", "KA-53-TR1234"),
+            Car("Violet", "KA-50-MB9021"),
+            Car("Brown", "GJ-01-EW1234")
+        )
+        val stdoutMessage =
+            cars.mapIndexed { index, car: Car -> "Parking Car with License Plate: ${car.licensePlate} and color: ${car.color} at position $index" }
+                .joinToString("\n")
+
+        cars.forEach { car -> parkingLot.parkCar(car) }
+        assertEquals(stdoutMessage, outContent.toString().trim())
+    }
+}
