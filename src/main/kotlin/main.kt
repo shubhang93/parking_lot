c4eec4fcfbc43db6a148463a51abadaf6679bae2
shubhang <shubhang.sabbath@gmail.com>
2019-11-06 15:44:22 +0530

    feat: add method to park car

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,18 @@
 data class ParkingSpot(val position: Int, val car: Car)
 data class Car(val color: String, val licensePlate: String)
 
-class ParkingLot(val capacity: Int) {
-    val availableSpots = mutableListOf<ParkingSpot>()
+class ParkingLot(private val capacity: Int) {
+    private val availableSpots = mutableListOf<ParkingSpot>()
 
+
+    fun isParkingLotFull(): Boolean {
+        return availableSpots.size == capacity
+    }
 
     fun parkCar(car: Car) {
-
+        val parkingSpot = ParkingSpot(1, car)
+        availableSpots.add(parkingSpot)
+        println("Parking Car with License Plate: ${car.licensePlate} and color: ${car.color} at position ${parkingSpot.position}")
     }
 
 }
