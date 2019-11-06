724e79c0fd555ff9bb8f20364644bd4442616a1f
shubhang <shubhang.sabbath@gmail.com>
2019-11-06 15:39:20 +0530

    fix: typo in data class name

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
-data class ParkingSpots(val position: Int, val car: Car)
+data class ParkingSpot(val position: Int, val car: Car)
 data class Car(val color: String, val licensePlate: String)
 
 class ParkingLot(val capacity: Int) {
-    val availableSpots = mutableListOf<ParkingSpots>()
+    val availableSpots = mutableListOf<ParkingSpot>()
 
 
     fun parkCar(car: Car) {
