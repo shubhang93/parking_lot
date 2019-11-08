f7ab7c5aa1c04a8f351c0784f4006720ba672d6a
shubhang <shubhang.sabbath@gmail.com>
2019-11-08 23:34:57 +0530

    feat: add regex for interactive mode input

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,6 +3,9 @@ import services.ParkingLotServiceImpl
 
 val CREATE_COMMAND_REGEX = Regex("""create_parking_lot \d+""")
 val PARK_COMMAND_REGEX = Regex("""park [a-zA-Z]{2}-\d{2}-[a-zA-Z]{2}-\d{4} [a-zA-Z]+""")
+val UNPARK_COMMAND_REGEX = Regex("""leave \d+""")
+val REG_NUMS_FOR_COLOR_QUERY = Regex("""registration_numbers_for_cars_with_colour [a-zA-Z]+""")
+val SLOT_NUM_FOR_REG_NUM_QUERY = Regex("""slot_number_for_registration_number [a-zA-Z]+""")
 
 
 fun runInteractiveCli(parkingLotServiceImpl: ParkingLotServiceImpl) {
