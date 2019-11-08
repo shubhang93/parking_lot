3a85971f5f358612416cf31f1356c7a3deb49ef4
shubhang <shubhang.sabbath@gmail.com>
2019-11-08 23:31:35 +0530

    feat: add regex to parse command line

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,24 @@
-enum class Command(val command: String) {
-    CREATE_PARKING_LOT("create_parking_lot"),
-    PARK_CAR("park")
+import services.ParkingLotServiceImpl
+
+
+val CREATE_COMMAND_REGEX = Regex("""create_parking_lot \d+""")
+val PARK_COMMAND_REGEX = Regex("""park [a-zA-Z]{2}-\d{2}-[a-zA-Z]{2}-\d{4} [a-zA-Z]+""")
+
+
+fun runInteractiveCli(parkingLotServiceImpl: ParkingLotServiceImpl) {
+    loop@ while (true) {
+        val userInput: String? = readLine()
+        when {
+            (userInput == "exit") -> break@loop
+
+            else -> println("Invalid Command, please try again")
+
+        }
+    }
 }
 
 fun main(args: Array<String>) {
+    val parkingService = ParkingLotServiceImpl.initializeParkingLot(6)
     when (args.size) {
         0 -> {
             // Interactive Mode
