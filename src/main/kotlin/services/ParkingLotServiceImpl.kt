a525d60d511c393cbf7decbbfb5fff9fc30ad885
shubhang <shubhang.sabbath@gmail.com>
2019-11-09 13:46:04 +0530

    fix: unpark message

--- src/main/kotlin/services/ParkingLotServiceImpl.kt
+++ src/main/kotlin/services/ParkingLotServiceImpl.kt
@@ -4,13 +4,16 @@ import dataManager.IParkingLot
 import domain.Car
 import domain.ParkingSpot
 
-class ParkingLotService(parkingLot: IParkingLot) : IParkingLotService {
+class ParkingLotService(val parkingLot: IParkingLot) : IParkingLotService {
     override fun park(car: Car) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val parkingSpot = parkingLot.park(car)
+        if (parkingSpot != null) println("Allocated slot number: ${parkingSpot.position}")
+        else println("Sorry, parking lot is full")
     }
 
     override fun unParkCar(parkingSpot: ParkingSpot) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val freeSpot = parkingLot.unpark(parkingSpot)
+        println("Slot number ${freeSpot.position} is free")
     }
 
     override fun getStatus(): List<Map<String, String>> {
