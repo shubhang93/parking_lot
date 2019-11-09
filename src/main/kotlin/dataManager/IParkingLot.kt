06ff3a63d8d34cd9b7df765b229b68c4a23827da
shubhang <shubhang.sabbath@gmail.com>
2019-11-09 13:44:40 +0530

    fix: return type

--- src/main/kotlin/dataManager/IParkingLot.kt
+++ src/main/kotlin/dataManager/IParkingLot.kt
@@ -5,7 +5,7 @@ import domain.ParkingSpot
 
 interface IParkingLot {
     fun park(car: Car): ParkingSpot?
-    fun unpark(parkingSpot: ParkingSpot): ParkingSpot?
+    fun unpark(parkingSpot: ParkingSpot): ParkingSpot
     fun getOccupiedSlots(): Map<ParkingSpot, Car>
     fun getFreeSlots(): List<ParkingSpot>
 }
