a19116fe3c8dc082354cf500490f600fd46009d7
shubhang <shubhang.sabbath@gmail.com>
2019-11-09 23:09:03 +0530

    feat: add test to check parking lot initialization

--- src/test/kotlin/ParkingLotTest.kt
+++ src/test/kotlin/ParkingLotTest.kt
@@ -5,8 +5,16 @@ import org.junit.Test
 class ParkingLotTest {
     @Test
     fun itShouldCreateASingleTonObject() {
-        val parkingLot1 = InMemoryIParkingLot.createParkingLot(6)
-        val parkingLot2 = InMemoryIParkingLot.createParkingLot(2)
+        val parkingLot1 = InMemoryIParkingLot().createParkingLot(6)
+        val parkingLot2 = InMemoryIParkingLot().createParkingLot(2)
         assertEquals(parkingLot1, parkingLot2)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun itShouldInitializeExactlyOnce() {
+        val inMemoryIParkingLot = InMemoryIParkingLot()
+        inMemoryIParkingLot.createParkingLot(6)
+        inMemoryIParkingLot.createParkingLot(5)
+
     }
 }
