e8bf204a0b4b155d9dc96258dc001fd2e64e312a
shubhang <shubhang.sabbath@gmail.com>
2019-11-09 11:45:49 +0530

    fix: interface method arguments

--- src/main/kotlin/domain/IParkingLot.kt
+++ src/main/kotlin/domain/IParkingLot.kt
@@ -2,7 +2,7 @@ package domain
 
 interface IParkingLot {
     fun park(car: Car): ParkingSpot?
-    fun unpark()
+    fun unpark(parkingSpot: ParkingSpot): ParkingSpot?
     fun getOccupiedSlots(): Map<ParkingSpot, Car>
     fun getFreeSlots(): List<ParkingSpot>
 }
