6e1256cf97a8eb01d0f9875f7cc280c2196f5693
shubhang <shubhang.sabbath@gmail.com>
2019-11-10 19:14:51 +0530

    feat: update test

--- src/test/kotlin/ParkingLotServiceTest.kt
+++ src/test/kotlin/ParkingLotServiceTest.kt
@@ -1,8 +1,6 @@
 import dataManager.InMemoryIParkingLot
 import domain.Car
-import domain.ParkingSpot
 import org.junit.*
-import org.junit.Assert.assertEquals
 import org.junit.runners.MethodSorters
 import services.ParkingLotService
 import java.io.ByteArrayOutputStream
@@ -24,6 +22,14 @@ class ParkingLotServiceTest {
         fun setup() {
             parkingLotService = ParkingLotService(InMemoryIParkingLot())
             parkingLotService.createParkingLot(PARKING_LOT_CAPACITY)
+            val cars = listOf(
+                Car("White", "KA-01-AB-1234"),
+                Car("White", "KA-02-BD-9089"),
+                Car("Blue", "KA-05-GJ-8765")
+            )
+            cars.forEach {
+                parkingLotService.park(it)
+            }
         }
 
         @AfterClass
@@ -45,51 +51,6 @@ class ParkingLotServiceTest {
     }
 
 
-    @Test
-    fun AitShouldInitializeParkingLot() {
-        assertEquals(PARKING_LOT_CAPACITY, parkingLotService.getFreeSlotCount())
-    }
 
 
-    @Test
-    fun BitShouldParkCars() {
-        val cars = listOf(
-            Car("White", "KA-02-EU8901"),
-            Car("Blue", "KA-01-AE9099"),
-            Car("Yellow", "KA-02-DV8900"),
-            Car("Orange", "KA-53-TR1234"),
-            Car("Violet", "KA-50-MB9021"),
-            Car("Brown", "GJ-01-EW1234")
-        )
-        val stdoutMessage =
-            cars.mapIndexed { index, _: Car -> "Allocated slot number: ${index + 1}" }
-                .joinToString("\n")
-
-        cars.forEach { car -> parkingLotService.park(car) }
-        assertEquals(stdoutMessage, outContent.toString().trim())
-    }
-
-    @Test
-    fun CshouldBecomeFullAfterCapacityIsExhausted() {
-        val cars = listOf(
-            Car("Yellow", "KA-04-EM2123"),
-            Car("Orange", "KA-21-EF4567"),
-            Car("Blue", "KA-06-DS2123")
-        )
-        cars.forEach { car -> parkingLotService.park(car) }
-        val freeSlots = parkingLotService.getFreeSlotCount()
-        assertEquals(0, freeSlots)
-    }
-
-    @Test
-    fun DitShouldUnParkCar() {
-        parkingLotService.unParkCar(ParkingSpot(2))
-        assertEquals("Slot number 2 is free", outContent.toString().toString().trim())
-    }
-
-    @Test
-    fun EitShouldReleaseParkingSpotAfterUnPark() {
-        parkingLotService.unParkCar(ParkingSpot(1))
-        assertEquals(2, parkingLotService.getFreeSlotCount())
-    }
 }
