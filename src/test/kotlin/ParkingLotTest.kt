870836f3adbea017ec985c7298ac1121ed213292
shubhang <shubhang.sabbath@gmail.com>
2019-11-10 11:30:39 +0530

    fix: test execution order

--- src/test/kotlin/ParkingLotTest.kt
+++ src/test/kotlin/ParkingLotTest.kt
@@ -7,7 +7,7 @@ import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runners.MethodSorters
 
-@FixMethodOrder(MethodSorters.JVM)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ParkingLotTest {
 
     companion object {
@@ -21,14 +21,13 @@ class ParkingLotTest {
 
 
     @Test(expected = IllegalStateException::class)
-    fun itShouldInitializeExactlyOnce() {
+    fun AitShouldInitializeExactlyOnce() {
         inMemoryIParkingLot.createParkingLot(6)
         inMemoryIParkingLot.createParkingLot(5)
-
     }
 
     @Test
-    fun itShouldParkSixCars() {
+    fun BitShouldParkSixCars() {
         val cars = listOf(
             Car("White", "KA-02-EU8901"),
             Car("Blue", "KA-01-AE9099"),
@@ -60,7 +59,7 @@ class ParkingLotTest {
     }
 
     @Test
-    fun itShouldUnParkCarsAtSpotFourAndTwo() {
+    fun CitShouldUnParkCarsAtSpotFourAndTwo() {
         val positions = listOf<Int>(2, 4)
         val expected = listOf<ParkingSpot>(ParkingSpot(2), ParkingSpot(4))
         val results = mutableListOf<ParkingSpot>()
@@ -72,11 +71,26 @@ class ParkingLotTest {
     }
 
     @Test
-    fun itShouldAllocateTheNearestSpot() {
+    fun DitShouldAllocateTheNearestSpot() {
         val expected = ParkingSpot(2)
         val car = Car("Maroon", "KA-02-AM9098")
         val result = inMemoryIParkingLot.park(car)
         assertEquals(expected, result)
+    }
+
+    @Test
+    fun EitShouldReturnParkingLotStatus() {
+        val expected: Map<ParkingSpot, Car> = mapOf(
+            ParkingSpot(1) to Car("White", "KA-02-EU8901"),
+            ParkingSpot(3) to Car("Yellow", "KA-02-DV8900"),
+            ParkingSpot(5) to Car("Violet", "KA-50-MB9021"),
+            ParkingSpot(6) to Car("Brown", "GJ-01-EW1234"),
+            ParkingSpot(2) to Car("Maroon", "KA-02-AM9098")
+            )
+
+        val result = inMemoryIParkingLot.getOccupiedSlots()
+        assertEquals(expected, result)
+
     }
 
 
