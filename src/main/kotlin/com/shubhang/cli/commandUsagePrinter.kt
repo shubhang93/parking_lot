1e2557ad4a87862513b5ebb3d93563c97a0eb2df
shubhang <shubhang.sabbath@gmail.com>
2019-11-11 12:28:53 +0530

    feat: print usage in a more readable fashion

--- src/main/kotlin/com/shubhang/cli/commandUsagePrinter.kt
+++ src/main/kotlin/com/shubhang/cli/commandUsagePrinter.kt
@@ -3,14 +3,17 @@ package com.shubhang.cli
 fun commandUsagePrinter() {
     val usage = """
         Welcome to Parking Lot CLI
-        - To start create a parking lot with desired capacity using the command: create_parking_lot <capacity>
-        - To park a car enter: park <car_license_plate_number> <color>
-        - To view the parking lot status enter: status
-        - To free a parking spot enter: leave <slot_number>
-        - To view license plate numbers of cars for a specific color enter: registration_numbers_for_cars_with_colour <color>
-        - To view slots occupied by a specific car color enter: slot_numbers_for_cars_with_colour <color>
-        - To view a slot occupied by a car with a specific license plate number enter: slot_number_for_registration_number <license_plate_number>
-        - To Quit the CLI enter: exit
+        Command                                                         Description
+        ------------------------------------------------------------------------------------------------------------------------------
+        create_parking_lot <capacity>                                   Create A parking log with desired capacity
+        park <license_plate_number> <colour>                            Allocate a parking slot for a Car
+        leave <slot_number>                                             Free up a parking slot at the desired position/slot
+        status                                                          View the current status of the parking lot
+        registration_numbers_for_cars_with_colour <colour>              View license plate numbers of all cars for a specific color
+        slot_numbers_for_cars_with_colour <colour>                      View slot numbers where cars with a specific color are parked
+        slot_number_for_registration_number <license_plate_number>      View slot number for a particular car
+        exit                                                            Exit the CLI
+        
     """.trimIndent()
     println(usage)
 }
