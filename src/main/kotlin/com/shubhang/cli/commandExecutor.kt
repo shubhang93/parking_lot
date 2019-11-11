82cc6d9b0529073d294ad717fd9a4c88eadf3633
shubhang <shubhang.sabbath@gmail.com>
2019-11-11 09:15:08 +0530

    refact: change service name

--- src/main/kotlin/com/shubhang/cli/commandExecutor.kt
+++ src/main/kotlin/com/shubhang/cli/commandExecutor.kt
@@ -10,11 +10,11 @@ fun commandParser(userInput: String, regex: Regex): List<String> {
 }
 
 
-fun commandExecutor(userInput: String, parkingLotService: ParkingService) {
+fun commandExecutor(userInput: String, parkingService: ParkingService) {
     when {
         (userInput.matches(CREATE_COMMAND_REGEX)) -> {
             val (_, capacity) = commandParser(userInput, CREATE_COMMAND_REGEX)
-            parkingLotService.createParkingLot(capacity.toInt())
+            parkingService.createParkingLot(capacity.toInt())
             println("Created a parking lot with $capacity slots")
         }
         (userInput.matches(PARK_COMMAND_REGEX)) -> {
@@ -22,32 +22,32 @@ fun commandExecutor(userInput: String, parkingLotService: ParkingService) {
                 userInput,
                 PARK_COMMAND_REGEX
             )
-            parkingLotService.park(Car(color, licensePlate))
+            parkingService.park(Car(color, licensePlate))
         }
         (userInput.matches(UNPARK_COMMAND_REGEX)) -> {
             val (_, slotNumber) = commandParser(userInput, UNPARK_COMMAND_REGEX)
-            parkingLotService.unParkCar(ParkingSpot(slotNumber.toInt()))
+            parkingService.unParkCar(ParkingSpot(slotNumber.toInt()))
         }
         (userInput.matches(STATUS_COMMAND_REGEX)) -> {
-            parkingLotService.printStatus()
+            parkingService.printStatus()
         }
         (userInput.matches(SLOT_NUMS_FOR_COLOR_QUERY_REGEX)) -> {
             val (_, color) = commandParser(userInput, SLOT_NUMS_FOR_COLOR_QUERY_REGEX)
-            parkingLotService.printSlotsByColor(color)
+            parkingService.printSlotsByColor(color)
         }
         (userInput.matches(SLOT_NUM_FOR_REG_NUM_QUERY_REGEX)) -> {
             val (_, licensePlateNumber) = commandParser(
                 userInput,
                 SLOT_NUM_FOR_REG_NUM_QUERY_REGEX
             )
-            parkingLotService.printSlotNumberByLicensePlate(licensePlateNumber)
+            parkingService.printSlotNumberByLicensePlate(licensePlateNumber)
         }
         (userInput.matches(REG_NUMBERS_FOR_COLOR_QUERY_REGEX)) -> {
             val (_, color) = commandParser(
                 userInput,
                 REG_NUMBERS_FOR_COLOR_QUERY_REGEX
             )
-            parkingLotService.printRegistrationNumbersForColor(color)
+            parkingService.printRegistrationNumbersForColor(color)
         }
         else -> println("Invalid Command, please try again")
     }
